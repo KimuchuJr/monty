9cbb2d1067933ecd1316abeb08a86630e55d42e2
KimuchuJr <kimuchukim@gmail.com>
2023-06-23 12:25:04 -0700

    Header file

---
+++ monty.h
@@ -0,0 +1,22 @@
+#ifndef MONTY_INTERPRETER_H
+#define MONTY_INTERPRETER_H
+
+#include <stdio.h>
+
+/* Doubly linked list node structure */
+typedef struct stack_s
+{
+    int n;
+    struct stack_s *prev;
+    struct stack_s *next;
+} stack_t;
+
+stack_t *createStack(void);
+void push(stack_t **stack, int value);
+void pall(stack_t **stack, unsigned int line_number);
+void freeStack(stack_t **stack);
+void processFile(const char *file_path);
+void parseInstruction(char *line, unsigned int line_number, stack_t **stack);
+void exeinst(char *opcode, unsigned int line_number, stack_t **stack);
+
+#endif /* MONTY_INTERPRETER_H */
